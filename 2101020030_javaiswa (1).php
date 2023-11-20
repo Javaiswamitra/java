a923e3053082080b015b6089a1545c7bdf29389b
Javaiswamitra <97436680+Javaiswamitra@users.noreply.github.com>
2023-11-20 15:16:09 +0800

    Add files via upload

---
+++ 2101020030_javaiswa (1).php
@@ -0,0 +1,12 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020030_Javaiswa";
+
+    $con = mysqli_connect($server,$user,$password,$database)
+
+?>
+
+
